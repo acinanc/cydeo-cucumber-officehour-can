55702f45fa812db025ed0e50f6be28b2ea7c8f90
Ali Can <ainanc@gmail.com>
2023-02-24 22:04:05 -0500

    new junit project created

---
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.cydeo.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html"
+
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        tags = "@validLoginWithParams",
+        dryRun = false,
+        publish = true  //in order to generate public link for reports
+
+)
+
+public class CukesRunner {
+}
