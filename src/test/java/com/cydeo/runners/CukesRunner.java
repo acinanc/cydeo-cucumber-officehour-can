0adc7ade5dfbdbce7254a832236538deedad32e5
Ali Can <ainanc@gmail.com>
2023-03-02 21:06:57 -0500

    rerun

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html"
+        plugin = {"html:target/cucumber-report.html",
+                "rerun:target/rerun.txt"
 
         },
         features = "src/test/resources/features",
